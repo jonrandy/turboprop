7cc137cb22e5fb1f4b710076d8dd6a292c5f6e3f
Jon Randy <jonrandy@gmail.com>
2022-05-04 15:10:00 +0700

    Adds object picking

--- picko.js
+++ picko.js
@@ -2,18 +2,21 @@ const pickoConcatMethod = Symbol('method')
 const pickoBase = Symbol('base')
 
 const defaultTargets = [
-	[String.prototype, (str,i) => str+i, ''],
-	[Array.prototype, (arr, i) => [...arr, i], []]
+	[String.prototype, (str, i) => str+i, ''],
+	[Array.prototype, (arr, i) => [...arr, i], []],
+	[Object.prototype, (obj, i, key) => ({...obj, [key]:i}), {}]
 ]
 
 export default function picko(source=[], targets=defaultTargets) {
+
+	if (!Array.isArray(source)) source = [source]
 
 	source[Symbol.toPrimitive] = function() {
 		const keys = this
 		const tempSym = Symbol()
 		const pickKeys = function () {
 			const ret = keys.reduce((all, key) => {
-				return this[pickoConcatMethod](all, this[key])
+				return this[pickoConcatMethod](all, this[key], key)
 			}, this[pickoBase])
 			targets.forEach(([target]) => {
 				delete target[tempSym]
