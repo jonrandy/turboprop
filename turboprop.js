ecf82735a3e75fbf703e377324715c8ad18c7618
Jon Randy <jonrandy@gmail.com>
2022-05-19 10:50:22 +0700

    Promotes refactored version

--- turboprop.js
+++ turboprop.js
@@ -1,44 +1,63 @@
 const
-	turbopropConcatMethod = Symbol('method'),
-	turbopropBase = Symbol('base'),
-	turbopropMarker = Symbol('turboprop')
+	TURBOPROP_MARKER = Symbol('turboprop'),
+	TURBOPROP_METHODS = Symbol('methods')
 
-const defaultTargets = [
-	[String.prototype, '', (str, i) => str+i],
-	[Array.prototype, [], (arr, i) => [...arr, i]],
-	[Object.prototype, {}, (obj, i, key) => ({...obj, [key]:i})]
+const
+	TARGET_STRING = {
+		object: String.prototype,
+		methods: {
+			getter: (keys, str) => keys.reduce((newStr, key) => newStr + str[key], ''),
+			setter: false
+		}
+	},
+	TARGET_ARRAY = {
+		object: Array.prototype,
+		methods: {
+			getter: (keys, arr) => keys.reduce((newArr, key) => [...newArr, arr[key]], []),
+			setter: (keys, values, arr) => keys.forEach((key, i) => arr[key] = values[i])
+		}
+	},
+	TARGET_OBJECT = {
+		object: Object.prototype,
+		methods: {
+			getter: (keys, arr) => keys.reduce((newArr, key) => [...newArr, arr[key]], []),
+			setter: (keys, values, arr) => keys.forEach((key, i) => arr[key] = values[i])
+		}
+	}
+ 
+const DEFAULT_TARGETS = [
+	TARGET_STRING,
+	TARGET_ARRAY,
+	TARGET_OBJECT
 ]
 
-export default function turboprop(source=[], targets=defaultTargets) {
+export default function turboprop(source = [], targets = DEFAULT_TARGETS) {
 
 	if (!Array.isArray(source)) source = [source]
 
-	source[turbopropMarker] = true
+	source[TURBOPROP_MARKER] = true
+
 	source[Symbol.toPrimitive] = function () {
-		const keys = this
-		const tempSym = Symbol()
+
+		const
+			keys = this.map(key => (Array.isArray(key) && !isTurbopropArray(key)) ? turboprop(key, targets) : key),
+			tempSym = Symbol(),
+			removeTempSymbols = () => targets.forEach(({object}) => delete object[tempSym])
+
 		const get = function () {
-			const ret = keys.reduce((all, key) => {
-				if (Array.isArray(key) && !isTurbopropArray(key)) key = turboprop(key) // Will also need to check here if global turboprop is switched on - if so, don't do this
-				return this[turbopropConcatMethod](all, this[key], key)
-			}, this[turbopropBase])
-			targets.forEach(([target]) => {
-				delete target[tempSym]
-			})
+			const ret = this[TURBOPROP_METHODS].getter(keys, this)
+			removeTempSymbols()
 			return ret
 		}
+
 		const set = function (values) {
-			keys.forEach((key, i) => {
-				this[key] = values[i]
-			})
-			targets.forEach(([target]) => {
-				delete target[tempSym]
-			})
+			this[TURBOPROP_METHODS].setter && this[TURBOPROP_METHODS].setter(keys, values, this)
+			removeTempSymbols()
 		}
-		targets.forEach(([target, base, concatMethod]) => {
-			Object.defineProperty(target, tempSym, { configurable: true, get, set})
-			target[turbopropConcatMethod] = concatMethod
-			target[turbopropBase] = base
+
+		targets.forEach(({object, methods}) => {
+			Object.defineProperty(object, tempSym, { configurable: true, get, set})
+			object[TURBOPROP_METHODS] = methods
 		})
 		return tempSym
 	}
@@ -47,4 +66,4 @@ export default function turboprop(source=[], targets=defaultTargets) {
 
 }
 
-const isTurbopropArray = toCheck => toCheck[turbopropMarker]
+const isTurbopropArray = toCheck => toCheck[TURBOPROP_MARKER]
